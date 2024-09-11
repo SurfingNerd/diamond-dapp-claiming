ea621db262e4f9acd93abbac50ebf51e83233cfd
MSalman6 <salman666online@gmail.com>
2024-09-11 18:23:36 +0500

    added all required functionality

--- src/components/ClaimForm/index.tsx
+++ src/components/ClaimForm/index.tsx
@@ -1,23 +1,26 @@
 import BN from "bn.js";
 import { ethers } from "ethers";
 import { toast } from 'react-toastify';
-import styles from "./styles.module.css";
 import { MESSAGES } from "../../constants/messages";  
 import { useEffect, useRef, useState } from "react";
 import { useRootContext } from "../../contexts/RootContext/RootContext";
 import { ensure0x } from "diamond-contracts-claiming/dist/api/src/cryptoHelpers";
 
-const ClaimForm = () => {
+const DmdDiamondClaiming = () => {
   const {
+    account,
     claimApi,
     showLoader,
+    connectWallet,
     getClaimTxHash,
     handleErrorMsg,
     ensureWalletConnection,
   } = useRootContext();
   
+  const [darkMode, setDarkMode] = useState(false);
   const [v3Address, setV3Address] = useState("");
   const [v4Address, setV4Address] = useState("");
+  const [copySuccess, setCopySuccess] = useState(false);
   const [signedMessage, setSignedMessage] = useState("");
   const [claimError, setClaimError] = useState<string>("");
   const [claimedTxHash, setClaimedTxHash] = useState<string>("");
@@ -65,7 +68,7 @@ const ClaimForm = () => {
       });
     } catch(err: any) {
       if (err.message.includes("Non-base58") || err.message.includes("Invalid")) {
-        toast.error(MESSAGES.invalidAddress);
+        // toast.error(MESSAGES.invalidAddress);
       } else {
         console.log("[ERROR] Err", err);
       }
@@ -110,11 +113,6 @@ const ClaimForm = () => {
     });
   };
 
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(claimMessagePrefix);
-    toast.success(MESSAGES.copiedToClipboard);
-  };
-
   const handleV4AddressChange = (e: any) => {
     try {
       if (ethers.getAddress(e.target.value)) {
@@ -157,129 +155,144 @@ const ClaimForm = () => {
     }
   };
 
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    document.documentElement.setAttribute('data-theme', darkMode ? 'light' : 'dark');
+  };
+
+  const copyText = () => {
+    navigator.clipboard.writeText(claimMessagePrefix);
+    toast.success(MESSAGES.copiedToClipboard);
+  };
+
   return (
-    <div className={styles.navbar}>
-      <form className="claimForm" onSubmit={claimSubmit} onKeyDown={handleKeyDown}>
-        <div>
-          <input
-            placeholder="Please specify your v3 address"
-            onChange={(e) => setV3Address(e.target.value)}
-            required
-            minLength={34}
-            maxLength={34}
-          />
-
-          {validV3Address === false && (
-            <p className={styles.redText}>
-              {MESSAGES.invalidV3Address}
-            </p>
-          )}
-        </div>
-
-        {claimableBalance || fetchingBalance ? (
-          <div>
-            {
-              fetchingBalance ? (
-                <p>{MESSAGES.fetchingBalance}</p>
-              ) : (
-                <p>{MESSAGES.claimableDmd} {Number(claimableBalance).toFixed(2)} DMD</p>
-              )
-            }
-          </div>
-        ) : claimedTxHash ? claimSuccess ? (<div>
-          <p className={styles.greenText}>
-            Claimed successfully! Please check the transaction here{" "}
-            <a
-              href={process.env.REACT_APP_EXPLORER_TX_URL + claimedTxHash}
-              target="_blank"
-            >
-              [link]
-            </a>
-          </p>
-        </div>) :
-         (
-          <div>
-            <p className={styles.redText}>
-              {MESSAGES.alreadyClaimed}
-              <a
-                href={process.env.REACT_APP_EXPLORER_TX_URL + claimedTxHash}
-                target="_blank"
-              >
-                [link]
-              </a>
-            </p>
-          </div>
-        ) : (
-          <div>
-            <p className={styles.redText}>{claimError}</p>
-          </div>
-        )}
-
-        {claimableBalance && (
-          <div>
-            <input
-              placeholder="Please specify your v4 address"
-              onChange={(e) => handleV4AddressChange(e)}
-              required
-              minLength={42}
-              maxLength={42}
-            />
-            {validV4Address === false && (
-              <p className={styles.redText}>
-                {MESSAGES.invalidV4Address}
-              </p>
-            )}
-          </div>
-        )}
-
-        {validV4Address && claimableBalance && (
-          <div className={styles.inputContainer}>
-            <textarea
-              id="claimMessagePrefix"
-              value={claimMessagePrefix}
-              onChange={(e) => setClaimMessagePrefix(e.target.value)}
-            />
-            <button
-              type="button"
-              className={styles.copyButton}
-              onClick={copyToClipboard}
-            >
-              Copy
-            </button>
-          </div>
-        )}
-
-        {validV4Address && claimableBalance && (
-          <div>
-            <input
-              placeholder="Please provide the signature you've generated"
-              onChange={(e) => handleSignatureChange(e.target.value)}
-              required
-            />
-
-            <p className={styles.redText}>{signatureError}</p>
-          </div>
-        )}
-
-        {validV4Address && claimableBalance && signedMessage && (
-          <button disabled={
-            claimableBalance && !signatureError && parseFloat(claimableBalance) > -1
-              ? false
-              : true
-          } className={styles.claimButton}>
-            <div className={styles.svgWrapper1}>
-                <div className={styles.svgWrapper}>
-                    <span className={styles.diamond}>
-                        💎
-                    </span>
-                </div>
-            </div>
-            <span>Claim</span>
+    <div className={darkMode ? 'dark' : 'light'}>
+      <div className="navbar">
+        <a href="#">
+          User Guide: <strong>How to claim <span className="diamond">💎</span></strong>
+        </a>
+        <div className="btnsContainer">
+          <button onClick={toggleDarkMode} className="primaryBtn theme">
+            <i className={`fas ${darkMode ? 'fa-sun' : 'fa-cloud-moon'}`}></i>
           </button>
-        )}
-      </form>
+          <button className="primaryBtn walletConnectBtn" onClick={connectWallet}>{account ? account : 'Connect Wallet'}</button>
+        </div>
+      </div>
+
+      <div className="form-container">
+        <div className="step-indicator">
+          <div className={`step ${!claimableBalance && "active"}`}></div>
+          <div className={`step ${claimableBalance && "active"}`}></div>
+          <div className={`step ${claimableBalance && validV4Address && "active"}`}></div>
+          <div className={`step ${!claimableBalance && validV4Address && signedMessage && "active"}`}></div>
+        </div>
+        <div className="form-content">
+          <h1>Claim your V4 DMD Coins</h1>
+          <form onSubmit={claimSubmit} onKeyDown={handleKeyDown}>
+            <input
+              placeholder="Please specify your v3 address"
+              onChange={(e) => setV3Address(e.target.value)}
+              required
+              minLength={34}
+              maxLength={34} />
+
+            {validV3Address === false && (<p className="text-error">{MESSAGES.invalidV3Address}</p>)}
+
+            {claimableBalance || fetchingBalance ? (
+              <div>
+                {
+                  fetchingBalance ? (
+                    <p>{MESSAGES.fetchingBalance}</p>
+                  ) : (
+                    <p>{MESSAGES.claimableDmd} <strong>{Number(claimableBalance).toFixed(2)} DMD</strong></p>
+                  )
+                }
+              </div>
+            ) : claimedTxHash ? claimSuccess ? (<div>
+              <p className="text-success">
+                Claimed successfully! Please check the transaction here{" "}
+                <a
+                  href={process.env.REACT_APP_EXPLORER_TX_URL + claimedTxHash}
+                  target="_blank"
+                >
+                  [link]
+                </a>
+              </p>
+            </div>) :
+            (
+              <div>
+                <p className="text-error">
+                  {MESSAGES.alreadyClaimed}
+                  <a
+                    href={process.env.REACT_APP_EXPLORER_TX_URL + claimedTxHash}
+                    target="_blank"
+                  >
+                    [link]
+                  </a>
+                </p>
+              </div>
+            ) : (
+              <div><p className="text-error">{claimError}</p></div>
+            )}
+
+            {claimableBalance && (
+              <div>
+                <input
+                  placeholder="Please specify your v4 address"
+                  onChange={(e) => handleV4AddressChange(e)}
+                  required
+                  minLength={42}
+                  maxLength={42}
+                />
+                {validV4Address === false && (
+                  <p className="text-error">
+                    {MESSAGES.invalidV4Address}
+                  </p>
+                )}
+              </div>
+            )}
+
+            {validV4Address && claimableBalance && (
+              <div className="textarea-container">
+                <textarea
+                  id="claimMessagePrefix"
+                  value={claimMessagePrefix}
+                  onChange={(e) => setClaimMessagePrefix(e.target.value)}
+                />
+                <button type="button" onClick={copyText}>
+                  <i className="fas fa-copy"></i>
+                  <span className="tooltip">{copySuccess ? 'Copied!' : ''}</span>
+                </button>
+              </div>
+            )}
+
+            {validV4Address && claimableBalance && (
+              <div>
+                <input
+                  placeholder="Please provide the signature you've generated"
+                  onChange={(e) => handleSignatureChange(e.target.value)}
+                  required
+                />
+
+                <p className="text-error">{signatureError}</p>
+              </div>
+            )}
+
+            {validV4Address && claimableBalance && signedMessage && (
+              <button disabled={
+                claimableBalance && !signatureError && parseFloat(claimableBalance) > -1
+                  ? false
+                  : true
+              } className="primaryBtn" type="submit">
+                <span className="diamond">💎</span> Claim
+              </button>
+            )}
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default ClaimForm;
+export default DmdDiamondClaiming;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import "./index.css";
+import "./global.css";
 import App from "./App";
 import ReactDOM from "react-dom/client";
 import 'react-toastify/dist/ReactToastify.css';
@@ -8,6 +8,7 @@ import { RootContextProvider } from "./contexts/RootContext/RootContext";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <RootContextProvider>
     <App />
